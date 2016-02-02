279a01dd088dc04792b65a3e841cd994c6fa4087
Neil Jin <zhao.jin@liferay.com>
2016-02-02 17:07:05 +0800

    SF and logic change

--- languagedetector/language-detector/src/main/java/com/liferay/languagedetector/messaging/UserLanguageDetectorMessageListener.java
+++ languagedetector/language-detector/src/main/java/com/liferay/languagedetector/messaging/UserLanguageDetectorMessageListener.java
@@ -3,8 +3,6 @@ package com.liferay.languagedetector.messaging;
 import com.liferay.languagedetector.util.Constants;
 import com.liferay.languagedetector.util.PortletPropsValues;
 import com.liferay.portal.NoSuchUserException;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
@@ -26,10 +24,11 @@ public class UserLanguageDetectorMessageListener implements MessageListener {
 	@Override
 	public void receive(Message message) throws MessageListenerException {
 		long companyId = PortalUtil.getDefaultCompanyId();
-		
+
 		try {
 			User user = UserLocalServiceUtil.getUserByEmailAddress(
 				companyId, PortletPropsValues.DETECT_USER_EMAIL);
+
 			ExpandoTable table = ExpandoTableLocalServiceUtil.getDefaultTable(
 				user.getCompanyId(), User.class.getName());
 
@@ -47,19 +46,20 @@ public class UserLanguageDetectorMessageListener implements MessageListener {
 
 			String userLanguage = user.getLanguageId();
 
-			/*code logic
-			 * time is null, language is not en, -> add time stamp
-			 * time is null, language is en - > return(no need to implement)
-			 * time is not null, language is en -> delete time
-			 * time is not null, language is not en, less than 10, return
+			/* code logic
+			 * time is null, language is zh_CN, -> add time stamp
+			 * time is null, language is not zh_CN - > return
 			 * (no need to implement)
-			 * time is not null, language is not en, more/equal than 10,
-			 * set user languate to be en_US, remove timestamp from expando.
+			 * time is not null, language is not zh_CN -> remove time stamp
+			 * time is not null, language is zh_CN, less than 20, return
+			 * (no need to implement)
+			 * time is not null, language is zh_CN, more/equal than 20 min,
+			 * set user language to be en_US, remove timestamp from expando.
 			*/
 			long timeStamp = System.currentTimeMillis();
 
 			if (Validator.isNull(languageChangeTime) &&
-				(!userLanguage.equals(Constants.LANGUAGE_ID_EN_US))) {
+				userLanguage.equals(Constants.LANGUAGE_ID_ZH_CN)) {
 
 				ExpandoValueLocalServiceUtil.addValue(
 					table.getClassNameId(), table.getTableId(),
@@ -70,35 +70,34 @@ public class UserLanguageDetectorMessageListener implements MessageListener {
 			}
 
 			if (Validator.isNotNull(languageChangeTime) &&
-				userLanguage.equals(Constants.LANGUAGE_ID_EN_US)) {
+				(!userLanguage.equals(Constants.LANGUAGE_ID_ZH_CN))) {
 				ExpandoValueLocalServiceUtil.addValue(
 					table.getClassNameId(), table.getTableId(),
-					column.getColumnId(), user.getUserId(),
-					StringPool.BLANK);
+					column.getColumnId(), user.getUserId(), StringPool.BLANK);
 			}
 
-			if (Validator.isNotNull(languageChangeTime) 
-				&& (!userLanguage.equals(Constants.LANGUAGE_ID_EN_US))
-				&& (timeStamp - Long.valueOf(languageChangeTime) >= 600000)) {
+			if (Validator.isNotNull(languageChangeTime) &&
+				userLanguage.equals(Constants.LANGUAGE_ID_ZH_CN) &&
+				(timeStamp - Long.valueOf(languageChangeTime) >= 1200000)) {
 
 				user.setLanguageId(Constants.LANGUAGE_ID_EN_US);
 				UserLocalServiceUtil.updateUser(user);
 
 				ExpandoValueLocalServiceUtil.addValue(
 					table.getClassNameId(), table.getTableId(),
-					column.getColumnId(), user.getUserId(),
-					StringPool.BLANK);
+					column.getColumnId(), user.getUserId(), StringPool.BLANK);
+
 				_log.info("update user language to en_US");
 			}
 		} catch (NoSuchUserException nsue) {
-			_log.info("user " + PortletPropsValues.DETECT_USER_EMAIL 
-				+ " does not exist");
+			_log.info("user " + PortletPropsValues.DETECT_USER_EMAIL +
+				" does not exist");
 		}
 		catch (Exception e) {
 			_log.error(e);
 		}
 	}
 
-	private static Log _log =
-		LogFactoryUtil.getLog(UserLanguageDetectorMessageListener.class);
+	private static Log _log = LogFactoryUtil.getLog(
+		UserLanguageDetectorMessageListener.class);
 }
