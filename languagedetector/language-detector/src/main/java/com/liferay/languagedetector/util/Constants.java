c06eb8853c46764e87dffbd39a24ba6adf546bc4
Neil Jin <zhao.jin@liferay.com>
2016-02-04 11:34:25 +0800

    sf

--- languagedetector/language-detector/src/main/java/com/liferay/languagedetector/util/Constants.java
+++ languagedetector/language-detector/src/main/java/com/liferay/languagedetector/util/Constants.java
@@ -1,7 +1,10 @@
 package com.liferay.languagedetector.util;
 
 public class Constants {
+
 	public static final String EXPANDO_COLUMN_NAME = "languageChangeTime";
+
 	public static final String LANGUAGE_ID_EN_US = "en_US";
+
 	public static final String LANGUAGE_ID_ZH_CN = "zh_CN";
 }
