64a497799dfdc13ce65889fea73680346f18cb52
Neil Jin <zhao.jin@liferay.com>
2016-03-03 14:37:06 +0800

    super fast change

--- languagedetector/language-detector/src/main/java/com/liferay/languagedetector/util/PortletPropsKeys.java
+++ languagedetector/language-detector/src/main/java/com/liferay/languagedetector/util/PortletPropsKeys.java
@@ -2,4 +2,7 @@ package com.liferay.languagedetector.util;
 
 public class PortletPropsKeys {
 	public static final String DETECT_USER_EMAIL = "detect-user-email";
+	public static final String DETECT_LANGUAGE = "detect-language";
+	public static final String RECOVER_LANGUAGE = "recover-language";
+	public static final String LANGUAGE_LIFE_MS = "language-life-ms";
 }
